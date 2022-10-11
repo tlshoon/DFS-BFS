0bb4dc26893b7d2bb4a93ee8e4ed014b3635e3f9
JaeHoon <87574833+tlshoon@users.noreply.github.com>
2022-10-11 19:11:32 +0900

    Add files via upload

--- DFS,BFS.py
+++ DFS,BFS.py
@@ -163,39 +163,35 @@ print(graph)'''
 # 미로탈출
 # from collections import deque
 #
-# n, m = map(int, input().split())
+# n,m = map(int,input().split())
 #
 # graph = []
 # for i in range(n):
-#     graph.append(list(map(int, input())))
+#     graph.append(list(map(int,input())))
 #
-# check = [[0]*m for _ in range(n)]  # 방문 기록용 2차원 리스트
-#
-# count = [[0]*m for _ in range(n)]  # 이동칸 기록용 2차원 리스트
-#
-# dx = [-1,1,0,0]
+# dx = [-1,1,0,0]   # 이동할 네 방향 정의(상,하,좌,우)
 # dy = [0,0,-1,1]
 #
-# def bfs():
-#     queue = deque()
-#     queue.append((0,0))
-#     check[0][0] = 1
-#     count[0][0] = 1
 #
-#     while queue:
+# def bfs(x,y):
+#     queue = deque()  # 큐 구현을 위해 deque 라이브러리 사용
+#     queue.append((x,y))
+#     while queue:  # 큐가 빌 때까지 반복
 #         x,y = queue.popleft()
-#
-#         for i in range(4):
+#         for i in range(4):  # 현재 위치에서 네 방향으로의 위치 확인
 #             nx = x + dx[i]
 #             ny = y + dy[i]
 #
-#          if(nx >= 0 and nx < n and ny >= 0 and ny < m):
-#                 if(check[nx][ny] == 0 and graph[nx][ny] == 1):  # 인접한 노드를 방문할 수 있는지 확인
+#             if nx < 0 or nx >= n or ny < 0 or ny >= m:
+#                 continue
+#             if graph[nx][ny] == 0:  # 벽인 경우 무시
+#                 continue
+#             if graph[nx][ny] == 1:  # 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
+#                 graph[nx][ny] = graph[x][y] + 1
+#                 queue.append((nx,ny))
+#     return graph[n-1][m-1]
 #
-#                     queue.append((nx,ny))
-#                     check[nx][ny] = 1
-#                     count[nx][ny] = count[x][y] + 1
-#
-#     return count[n-1][m-1]
-#
-# print(bfs())
+# print(bfs(0,0))
+
+
+
