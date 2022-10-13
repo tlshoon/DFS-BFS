45a3c515bb2d6f6afcaf1140c90c6626f4b9fc4f
JaeHoon <87574833+tlshoon@users.noreply.github.com>
2022-10-13 15:38:55 +0900

    Add files via upload

--- DFS,BFS.py
+++ DFS,BFS.py
@@ -159,6 +159,36 @@ print(graph)'''
 #             count += 1
 # print(count)
 
+# 음료수 얼려먹기 다른 풀이
+# n,m = map(int,input().split())
+#
+# graph = []
+# for i in range(n):
+#     graph.append(list(map(int,input())))
+#
+#
+# def dfs(x,y):
+#     if x <= -1 or x >= n or y <= -1 or y >=m:
+#         return False
+#     if graph[x][y] == 0:
+#         graph[x][y] = 1
+#         dfs(x-1,y)
+#         dfs(x,y-1)
+#         dfs(x+1,y)
+#         dfs(x,y+1)
+#         return True
+#     return False
+#
+#
+# result = 0
+# for i in range(n):
+#     for j in range(m):
+#         if dfs(i,j) == True:
+#             print(i,j)
+#             result += 1
+# print(result)
+
+
 
 # 미로탈출
 # from collections import deque
